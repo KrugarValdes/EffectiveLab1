3647b3058fbc5345de4484bf9c014106cb574590
KrugarValdes <sm.vit2003@gmail.com>
2024-10-28 19:39:34 +0600

    7. Добавлена светлая тема

--- app/src/main/java/com/effectivelab1/heroapp/ui/theme/Theme.kt
+++ app/src/main/java/com/effectivelab1/heroapp/ui/theme/Theme.kt
@@ -1,57 +1,37 @@
 package com.effectivelab1.heroapp.ui.theme
 
-import android.app.Activity
-import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
-
-private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
-)
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import com.effectivelab1.heroapp.R
 
 @Composable
-fun Theme(
+fun HeroAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
-    val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+    val colors = if (darkTheme) {
+        darkColorScheme(
+            primary = colorResource(id = R.color.color_deadpool),
+            secondary = colorResource(id = R.color.color_ironman),
+            background = colorResource(id = R.color.app_background),
+            onSecondary = colorResource(id = R.color.white)
+        )
+    } else {
+        lightColorScheme(
+            primary = colorResource(id = R.color.color_spiderman),
+            secondary = colorResource(id = R.color.color_captainamerica),
+            background = colorResource(id = R.color.app_background_light),
+            onSecondary = Color.Black
+        )
     }
 
     MaterialTheme(
-        colorScheme = colorScheme,
+        colorScheme = colors,
         typography = Typography,
         content = content
     )
